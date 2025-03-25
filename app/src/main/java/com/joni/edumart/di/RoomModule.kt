645be79f72c0a533886d4fba79385ac6124986be
joni <jaunivashisth@gmail.com>
2025-03-26 01:55:34 +0530

    Changed App Icon

--- app/src/main/java/com/joni/edumart/di/RoomModule.kt
+++ app/src/main/java/com/joni/edumart/di/RoomModule.kt
@@ -16,7 +16,6 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object RoomModule {
 
-
     @Provides
     @Singleton
     fun provideRoomDatabase(@ApplicationContext context: Context): AppDatabase {
